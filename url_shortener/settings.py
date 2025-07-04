f2f818012246d4f5f1a9b7d8ab531fe9bd79d59d
beratcano <beratcanozkan@ogr.iu.edu.tr>
2025-07-04 20:40:32 +0300

    first deployment db fix #1

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -75,16 +75,10 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@db:5432/postgres'))
 }
 
 
