188f287d3b55188497b917039076bd3085d3a571
beratcano <beratcanozkan@ogr.iu.edu.tr>
2025-07-04 21:00:03 +0300

    first deployment db fix #3

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,7 +75,6 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import os
 import dj_database_url
 
 DATABASES = {
